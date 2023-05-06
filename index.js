125df61cf2a0bca4b1bfe65008b8a4b23b287177
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-05-06 18:49:08 +0530

    final-contact-form

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ $("#contact-form-button").click(function () {
 function sendMail(){
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "agamtyagi17@gmail.com",
-        Password : "8854FA9A52D7F4B59F288634EE954FA84420",
+        Username : "agamtyagi1717@gmail.com",
+        Password : "1ED861BADC8DD0DCF6CC2D953918E64981F9",
         To : 'agamtyagi1717@gmail.com',
-        From : document.getElementById("email").value,
+        From : 'agamtyagi1717@gmail.com',
         Subject : "New Contact Form Enquiry",
-        Body : "Name: "+ document.getElementById("name").value + "<br>Phone Number: " + document.getElementById("phone").value + "<br>Enquiry: " + document.getElementById("message").value
+        Body : "Name: "+ document.getElementById("name").value + "<br>Email: "+ document.getElementById("email").value + "<br>Phone Number: " + document.getElementById("phone").value + "<br>Enquiry: " + document.getElementById("message").value
     }).then(
     //   message => alert("Thank You! I'll get back to you ASAP")
         document.getElementById("send").innerHTML="Email Sent, thank you!",
